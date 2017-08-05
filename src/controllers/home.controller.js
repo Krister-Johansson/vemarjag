fe96536508adfb7d3a226075b46e47c641facbfb
Krister Johansson <krister.johansson86@outlook.com>
2017-08-05 14:31:43 +0200

    remove one function

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,17 +5,7 @@ const scraper = require('../business/scraper.busuness');
 
 module.exports = {
     home: (req, res, next) => {
-        group.list().then(x => {
-            if (x.length == 1) {
-                getRandomImage(req, res, next);
-            }
-            if (x.length == 0) {
-                return res.render('index');
-            }
-            if (x.length > 1) {
-                return res.render('index');
-            }
-        }).catch(x => console.log(x))
+        res.render('index');
     },
 
     getRandomImage: (req, res, next) => {
